c100fcf494d41064097f3eebfe1cd686ba20bcc0
Raja Shahpaal <91369872+Etzshally@users.noreply.github.com>
2024-07-03 16:27:36 +0500

    readme

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         API_URL: 'https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large',
-        HF_TOKEN: 'Bearer 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',
+        HF_TOKEN: 'PLACE YOUR HUGGINGFACE BEARER TOKEN HERE',
         DOCQAMODEL_API_URL: 'https://api-inference.huggingface.co/models/impira/layoutlm-document-qa'
     },
 };
